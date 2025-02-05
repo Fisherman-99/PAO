d80517a3f8bd40778d9ef0617cc819d18e7eebce
Fisherman-99 <danseal@yandex.ru>
2025-02-05 15:10:06 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+str = 'hydraulic'
+print(str[0])
+print(str[-1])
+print(str[2])
+print(str[-3])
+print(len (str))
+txt = "You won't find lead on the road."
+print(txt[0:8])
+centr = int((len(txt))/2)
+print (txt[centr - 2 : centr + 2])
+print(txt[0:len(txt):3])
+print(txt[::3])
